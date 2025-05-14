c62fad427fee27efffdf6b9f7b8b6705d9c73236
Yuri Matheus Barros <125932242+YuriMatheusBarros@users.noreply.github.com>
2025-05-14 13:40:47 -0300

    Corrige roteador para HashRouter e configura homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Intro from './intro';
 import QuestionScreen from './QuestionScreen';
 import ResultScreen from './ResultScreen';
@@ -19,5 +19,4 @@ function App() {
 }
 
 export default App;
-
 
