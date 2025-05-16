7a73681c2860fdb662b56b15620dc9bc27291c55
Yuri Matheus Barros <125932242+YuriMatheusBarros@users.noreply.github.com>
2025-05-16 11:30:32 -0300

    texto inicial atualizado

--- src/intro.js
+++ src/intro.js
@@ -24,10 +24,12 @@ function Intro() {
       <h1 className='boasVindas'>Bem-vindo ao Teste de Personalidade dos Cinco Fatores!</h1>
       <p className='explicacao-tracos'>Este teste é baseado no modelo dos cinco grandes traços de personalidade: <strong>Neuroticismo</strong>, <strong>Conscienciosidade</strong>, <strong>Extroversão</strong>, <strong>Agradabilidade</strong> e <strong>Abertura</strong>.</p>
 
+      
       <p className='explicacao'>O objetivo é avaliar como você se comporta e se sente em diferentes situações.</p>
-      <p className='explicacao'>O teste é composto por 33 perguntas, e você deve responder cada uma delas de acordo com a sua percepção.</p>
+      <p className='explicacao'>Composto por 33 perguntas, <span className='text-boaSorte'> este teste é apenas uma amostra de um teste real </span> — responda conforme sua própria percepção.</p>
       <p className='explicacao'>As opções de resposta variam entre: Discordo Completamente, Discordo parcialmente, Neutro, Concordo parcialmente, Concordo Completamente.</p>
-      <p className='explicacao'>Após responder todas as perguntas, você receberá um resultado detalhado sobre os seus traços de personalidade com maior destaque.<span className='text-boaSorte'>Boa Sorte!</span></p>
+      <p className='explicacao'> <span className='text-boaSorte'>É recomendavel evitar a opção neutro para maior exatidão do resultado.</span></p>
+      <p className='explicacao'>Após responder todas as perguntas, você receberá um resultado detalhado sobre os seus traços de personalidade com maior destaque.<span className='text-boaSorte'>Bom teste!</span></p>
 
       {/* Botão que inicia o teste e chama a função iniciarTeste */}
       <button className='button-iniciarTeste' onClick={iniciarTeste}>Iniciar Teste</button>
