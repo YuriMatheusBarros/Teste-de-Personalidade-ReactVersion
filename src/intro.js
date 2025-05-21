5ed8728495a17d8752d54dd09278421664f04d82
Cleissiane <cleissianeefonseca17@gmail.com>
2025-05-21 02:48:51 -0300

    Possivel conclusão da tela inicial

--- src/intro.js
+++ src/intro.js
@@ -9,7 +9,6 @@ import { useNavigate } from 'react-router-dom';
 import cerebroImg from './image/cerebro.png';
 import objetivoImg from './image/objetivo.png';
 import info from './image/info.png';
-import circuloAzul from './image/circuloAzul.png';
 
 // Criamos uma função chamada Intro, que é uma parte do site (uma página)
 function Intro() {
@@ -19,7 +18,7 @@ function Intro() {
   // Esta função é chamada quando clicamos no botão "Iniciar Teste"
   const iniciarTeste = () => {
     // Quando clicamos, ela nos leva para a página de perguntas ("/perguntas")
-    navigate('/perguntas'); 
+    navigate('/perguntas');
   };
 
   // Aqui está o que aparece na tela quando estamos nesta página
@@ -27,34 +26,42 @@ function Intro() {
     <div className="intro">
       {/* Textos explicando o que é o teste */}
       <div className='img-titulo'>
-        <img src={cerebroImg} alt="Ícone de cérebro"/>
+        <img src={cerebroImg} alt="Ícone de cérebro" />
         <h1 className='boasVindas'>Bem-vindo ao Teste de Personalidade dos Cinco Fatores!</h1>
       </div>
       <p className='explicacao-tracos'>Este teste é baseado no modelo dos cinco grandes traços de personalidade: <strong>Neuroticismo</strong>, <strong>Conscienciosidade</strong>, <strong>Extroversão</strong>, <strong>Agradabilidade</strong> e <strong>Abertura</strong>.</p>
 
       <div className='conjunto-img-texto'>
-        <img className='imagens-demonstrativas' src={objetivoImg} alt='Imagem de um alvo'/>
-        <p className='explicacao'> Objetivo: Avaliar como você reage em diferentes situações com base nos Cinco Fatores. Composto por 30 perguntas </p>
+        <img className='imagens-demonstrativas' src={objetivoImg} alt='Imagem de um alvo' />
+        <p className='explicacao negrito'> O objetivo é avaliar como você reage em diferentes situações com base nos Cinco Fatores.</p>
       </div>
 
       <div className='conjunto-img-texto'>
-        <img className='imagens-demonstrativas' src={info} alt='imagem de simbolo que representa informação'/>
-        <p className='explicacao'><em>Este é um teste de demonstração e não substitui uma avaliação psicológica profissional</em></p>
+        <img className='imagens-demonstrativas' src={info} alt='imagem de simbolo que representa informação' />
+        <p className='explicacao aviso'>Este é um teste de demonstração e não substitui uma avaliação psicológica profissional</p>
       </div>
 
       <div className='conjunto-img-texto'>
-        <img className='imagens-demonstrativas' src={circuloAzul} alt='Simbolo circulo azul'/>
-        <p className='explicacao'>As opções de resposta variam entre:</p>
+        <p className='explicacao'><em>&#10033; As opções de resposta variam entre:</em></p>
       </div>
-        <ul type='circle' className='opcoes'>
-          <li>Discordo Completamente</li>
-          <li>Discordo parcialmente</li>
-          <li>Neutro</li>
-          <li>Concordo parcialmente</li>
-          <li>Concordo Completamente</li>
-        </ul>
+      <ul type='circle' className='opcoes'>
+        <li>Discordo Completamente</li>
+        <li>Discordo parcialmente</li>
+        <li>Neutro</li>
+        <li>Concordo parcialmente</li>
+        <li>Concordo Completamente</li>
+      </ul>
 
-      <p className='explicacao '>Após responder todas as perguntas, você receberá um resultado detalhado sobre os seus traços de personalidade com maior destaque.<span className='text-cliqueParaComecar'>Pronto? Clique abaixo para começar!</span></p>
+      <div className='txt'>
+        <p className='explicacao txt-aposResponder'>Após responder todas as perguntas, você receberá um resultado detalhado sobre os seus traços de personalidade com maior destaque.</p>
+        <p className='text-cliqueParaComecar'>Pronto? Clique abaixo para começar!</p>
+      </div>
+
+      <div className="info-extra">
+        <p>&#x1F552; <strong>Duração estimada:</strong> 4 minutos</p>
+        <p>&#x1F9E9; <strong>Total de perguntas:</strong> 30</p>
+        <p>&#x1F512; <strong>Seus dados não são salvos</strong></p>
+      </div>
 
       <div className='divButton-iniciar'>
         {/* Botão que inicia o teste e chama a função iniciarTeste */}
